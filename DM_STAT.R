127127d4ae1e3fcb0a1fb02d2356abf55eef820b
Jonathan-Tutu <kyokoelol@gmail.com>
2022-05-29 15:30:58 +0200

    lala

--- DM_STAT.R
+++ DM_STAT.R
@@ -1,47 +1,47 @@
 assur <- read.csv("Data.csv",sep=";",dec=".",header=TRUE, row.names=1, stringsAsFactors=TRUE)
 head(assur)
 
-#Table de contigence
-test = table(assur$CSP, assur$Sexe)
-test
+#Table de contingence
+TabCSPSEXE = table(assur$CSP, assur$Sexe)
+TabCSPSEXE
 
-test2 = prop.table(test, margin=2) * 100 #permet d'y ramener sur un pourcentage
-test2
+propTable = prop.table(test, margin=2) * 100 #permet d'y ramener sur un pourcentage
+propTable
 
-barplot(test2, col =c("green4","red1","red2","red3","red4") ,
-        main="Niveau de satisfaction par test", xlim=c(0,5), legend=TRUE)
+bp <- barplot(propTable, col =c("mistyrose2","cadetblue3","bisque3","aquamarine3", "bisque1") ,
+        main="Répartition par CSP chez les hommes et les femmes", xlim=c(0,5), legend=TRUE)
 
 #Test du chi2
-chisq.test(test)
+chisq.test(TabCSPSEXE)
 
 
 #PARTIE B STUDENT
-dh <- subset(assur, assur$CSP == "Cadre") #On sépare en sous ensemble
-lala = table(dh$Salaire.annuel.net, dh$Sexe)
+subsetCadre <- subset(assur, assur$CSP == "Cadre") #On sépare en sous ensemble
+tabSalaireCadre = table(subsetCadre$Salaire.annuel.net, subsetCadre$Sexe)
 
-iris4 = subset(assur, Sexe == "Femme" & CSP == "Cadre")
-mean(iris4$Salaire.annuel.net)
-sd(iris4$Salaire.annuel.net)
-length(iris4$Salaire.annuel.net)
+FemmeCadre = subset(assur, Sexe == "Femme" & CSP == "Cadre")
+mean(FemmeCadre$Salaire.annuel.net)
+sd(FemmeCadre$Salaire.annuel.net)
+length(FemmeCadre$Salaire.annuel.net)
 
-iris5 = subset(assur, Sexe == "Homme" & CSP == "Cadre")
-mean(iris5$Salaire.annuel.net)
-sd(iris5$Salaire.annuel.net)
-length(iris5$Salaire.annuel.net)
+HommeCadre = subset(assur, Sexe == "Homme" & CSP == "Cadre")
+mean(HommeCadre$Salaire.annuel.net)
+sd(HommeCadre$Salaire.annuel.net)
+length(HommeCadre$Salaire.annuel.net)
 
-boxplot(iris4$Salaire.annuel.net, iris5$Salaire.annuel.net, whisklty = 1, boxfill=c("mistyrose2","cadetblue3", main="Re ́partition des clients suivant leur temps d’attente"))
-
-var.test(iris4$Salaire.annuel.net, iris5$Salaire.annuel.net)
+boxplot(FemmeCadre$Salaire.annuel.net, HommeCadre$Salaire.annuel.net, whisklty = 1, boxfill=c("mistyrose2","cadetblue3"), main="Distribution des salaires", ylab="Salaires en euros", legend=TRUE)
+       
+var.test(FemmeCadre$Salaire.annuel.net, HommeCadre$Salaire.annuel.net)
 
 #p-value > alpha Conserve H0 donc on dit que les variances sont égaux
 
-t.test(iris5$Salaire.annuel.net, iris4$Salaire.annuel.net,  alternative = "greater", var.equal=TRUE) #A vérifier
+t.test(HommeCadre$Salaire.annuel.net, FemmeCadre$Salaire.annuel.net,  alternative = "greater", var.equal=TRUE)
 #p-value < alpha donc on rejete H0 et oui machin est supérieur.
 
 
 #PARTIE C ANOVA
-iris6 = subset(assur, Sexe == "Femme")
-boxplot(iris6$Salaire.annuel.net~iris6$CSP, data=iris4, whisklty = 2, boxfill=c("mistyrose2","cadetblue3","bisque3","aquamarine3", "mistyrose3"), main="Répartition des clients suivant leur temps d’attente", legend=TRUE)
+SubsetFemme = subset(assur, Sexe == "Femme")
+boxplot(SubsetFemme$Salaire.annuel.net~SubsetFemme$CSP, data=SubsetFemme, whisklty = 2, boxfill=c("mistyrose2","cadetblue3","bisque3","aquamarine3", "bisque1"), main="Répartition des clients suivant leur temps d’attente", legend=TRUE)
 
 femmeOuv = subset(assur, Sexe == "Femme" & CSP == "Ouvrier")
 shapiro.test(femmeOuv$Salaire.annuel.net)
@@ -72,16 +72,16 @@ pairwise.t.test(iris6$Salaire.annuel.net, iris6$CSP, p.adjust.method = "fdr")
 install.packages("car")
 library("car")
 
-plot(Salaire.annuel.net ~ Age, data=iris4, pch=16)
-regmodel<-lm( Salaire.annuel.net ~ Age, data=iris4)
+plot(Salaire.annuel.net ~ Age, data=FemmeCadre, pch=16)
+regmodel<-lm( Salaire.annuel.net ~ Age, data=FemmeCadre)
 abline(regmodel)
 
-plot(iris4$Age, residuals(regmodel),pch=16,col="red")
+plot(FemmeCadre$Age, residuals(regmodel),pch=16,col="red")
 abline(h = 0)
 
 qqPlot(residuals(regmodel),pch=16)
 
-cor.test(iris4$Age, iris4$Salaire.annuel.net, alternative="greater", method="pearson")
+cor.test(FemmeCadre$Age, FemmeCadre$Salaire.annuel.net, alternative="greater", method="pearson")
 summary(regmodel)
 
 
